c8c1fe398cfa290029fe13babb96a631e22b6dbd
mjcod <mjcod@gmail.com>
2023-09-12 16:47:16 +0530

    table edit functionality

--- postgres/adventure_works/README.md
+++ postgres/adventure_works/README.md
@@ -11,7 +11,10 @@ make db_bootstrap # Adds the Seed Data to the Database
 
 ## Generate Data
 ```bash
-python generate_data.py --table <table_name> --num_records <number_of_rows_to_insert>
+# Create Records
+python generate_data.py --action create --table <table_name>  --num_records <number_of_rows_to_insert>
+# Edit Records
+python generate_data.py --action edit --table <table_name>  --num_records <number_of_rows_to_edit>
 ```
 #### Available Tables to Generate Data
 Valid tables so far:
--- postgres/adventure_works/src/generate_data.py
+++ postgres/adventure_works/src/generate_data.py
@@ -1,18 +1,20 @@
-import sys
 import argparse
 from logger import get_logger
 
-from crud import Data, loggy, metadata, GenerateData
+from crud import loggy, GenerateData
 
 loggy = get_logger(__name__)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--action", type=str,
+                        default='create', choices=['create', 'edit'])
     parser.add_argument("--table", type=str, default=None)
     parser.add_argument("--num_records", type=int, default=1)
     args, _ = parser.parse_known_args()
 
     loggy.info("Received arguments {}".format(args))
-                
-    generate_data = GenerateData(table=args.table, num_records=args.num_records)
+
+    generate_data = GenerateData(
+        action=args.action, table=args.table, num_records=args.num_records)
     generate_data.create_data()
